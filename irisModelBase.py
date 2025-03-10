258a4069705dded977d30e767a6c7d9b1836550e
108-0 <kbs5215553@g.shingu.ac.kr>
2025-03-10 16:02:34 +0900

    fastAPI 붓꽃분류 Get까지

--- irisModelBase.py
+++ irisModelBase.py
@@ -1,24 +1,37 @@
 # 2025_03_10
 #프로젝트 2 붓꽃분류기 만들기
+from fileinput import filename
+
+import joblib
 import numpy as np
 import pandas as pd
 from sklearn.neighbors import KNeighborsClassifier
-iris_df = pd.read_csv('iris.csv')
+from sklearn.ensemble import RandomForestClassifier
 
-#print(iris_df['sepal_length'].mean())
-Y=iris_df['species']
-X=iris_df.drop('species', axis=1)
-#print(X)
-#print(Y)
+# iris_df = pd.read_csv('iris.csv')
+#
+# #print(iris_df['sepal_length'].mean())
+# Y=iris_df['species']
+# X=iris_df.drop('species', axis=1)
+# #print(X)
+# #print(Y)
+#
+# kn =KNeighborsClassifier()
+# rfc = RandomForestClassifier()
+# model_kn = kn.fit(X,Y)
+# model_rfc = rfc.fit(X,Y)
+#
+# joblib.dump(model_rfc,'model_rfc.pkl')
 
-kn =KNeighborsClassifier()
-model_kn = kn.fit(X,Y)
+#X_new = np.array([[3,3,3,3]])
+X_new = np.array([[5.0,3.4,1.4,0.2]])
 
-X_new = np.array([[3,3,3,3]])
-#X_new = np.array([[5.0,3.4,1.4,0.2]])
 #['setosa']
 #[[1. 0. 0.]]
-prediction = model_kn.predict(X_new)
+model_rfc = joblib.load('model_rfc.pkl')
+# prediction = model_kn.predict(X_new)
+prediction = model_rfc.predict(X_new)
 print(prediction)
-probability = model_kn.predict_proba(X_new)
+# probability = model_kn.predict_proba(X_new)
+probability = model_rfc.predict_proba(X_new)
 print(probability)
